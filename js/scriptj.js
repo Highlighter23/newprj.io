76aa2351a3f06f4aeb3c83aa253c66aae9e00124
Andrii <homercast12@gmail.com>
2022-05-30 12:51:32 +0300

    first commit

---
+++ js/scriptj.js
@@ -0,0 +1,27 @@
+"use strict";
+result = prompt(title, [default]);
+/* const isChecked = true,
+      isClosed = false;
+      console.log(isChecked && !isClosed); */
+
+/* const category = 'toys';
+console.log(`https://someurl.com/${category}/5`) */
+/* const user = 'Andrii';
+
+alert (`Привіт, ${user}`); */
+/* const storeName = 'Papashon';
+const storeDescription = {
+    budget:10000,
+    employees:['Charles', 'Khabib', 'Justin'],
+    products:{
+        iphone:900,
+        samsung:800
+    },
+    open: true
+}; */
+/* const answers = [];
+answers[0] = prompt('Your name?', '');
+answers[1] = prompt('Your age?', '');
+answers[2] = prompt('Your phone?', '');
+document.write(answers);
+ */
