f5cfc7b0237c627e2a9af4e65d3092c5b4410532
Andrii <homercast12@gmail.com>
2022-05-31 20:39:25 +0300

    aft cycles

--- js/scriptj.js
+++ js/scriptj.js
@@ -10,15 +10,123 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-console.log(personalMovieDB);
+          if(a !=null && b!=null && a!='' && b !='' && a.length<50) {
+          personalMovieDB.movies[a] = b;   
+            console.log('done');
+        }  else {
+            console.log('error');
+            i--;
+        }
+}
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено двольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель!');
+}
+else if(personalMovieDB.count > 30) {
+    console.log('Вы - киноман!');
+}
+else{
+    console.log('Произошла ошибка'); }
+/* }
+/* function fifthTask() {
+    const arrayOfNumbers = [];
+
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
+}
+
+fifthTask() */
+
+
+/* console.log(personalMovieDB); */
+
+/* for (let i =2; i<=16; i++) {
+    if (i%2 === 0) {continue}
+    else {console.log(i)};
+} */
+
+/* let i = 2;
+while (i<=16) {
+    if (i%2 === 0) {i++;
+    continue} else{
+        console.log(i);
+    }
+    i++;
+}
+ */
+
+/* 
+first: for (let i = 1; i<3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 1; j<3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 1; k<3; k++) {
+            if(k===2) continue first
+            console.log(`Third level: ${k}`);
+}}} */
+/* for(let i=2; i<=10;i+=2){
+    console.log(i);
+}
+
+ */
+/* stop :for (let d=20; d--) {
+    if (d==10) break stop;
+    console.log(d);
+     */
+/* }; */
+/* for (let i = 5; i<= 10; i++) {
+    console.log(i);}
+     */
+/* const num = 50;
+switch(num) {
+    case 49:
+        console.log('Wrong!');
+        break;
+        case 56:
+        console.log('Wrong!');
+        break;
+        case 50:
+        console.log('Good!');
+        break;
+        default:
+        console.log('Try next time!');
+        break;
+
+}; */
+
+/* let result = '';
+const length = 7;
+console.log(result);
+for (let i = 1; i< length; i++) {
+    for (let j = 0 ; j < i; j++) {
+    result += '*'; }
+
+    result+= '\n';
+}
+console.log(result); */
+
+/* let nums = 50;
+
+while (nums<550) {
+
+console.log(nums);
+
+nums++;
+
+}
+
+ */
+
 
 
 /* const isChecked = true,
